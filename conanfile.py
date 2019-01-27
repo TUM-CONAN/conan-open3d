66e87535281299113744e2f7d5fbd3913c3f16d4
Ulrich Eck <ulrich.eck@tum.de>
2019-01-27 10:10:36 +0100

    added option to disable (default) glew/glfw requirements (open3d build with internal lib)

--- conanfile.py
+++ conanfile.py
@@ -17,16 +17,16 @@ class Open3dConan(ConanFile):
 
     requires = (
         "eigen/[>=3.3.4]@camposs/stable",
-        "glfw/[>=3.2.1]@camposs/stable",
-        "glew/[>=2.1.0]@camposs/stable",
         )
 
     options = {
         "shared": [True, False],
+        "with_visualization": [True, False],
         }
 
     default_options = (
         "shared=True",
+        "with_visualization=False",
         )
 
     scm = {
@@ -39,9 +39,15 @@ class Open3dConan(ConanFile):
 
     exports_sources = "CMakeLists.txt",
 
+    def requirements(self):
+        if self.options.with_visualization:
+            self.requires("glfw/[>=3.2.1]@camposs/stable")
+            self.requires("glew/[>=2.1.0]@camposs/stable")
+    
     def configure(self):
-        if self.options.shared:
-            self.options['glew'].shared = True
+        if self.options.with_visualization:
+            if self.options.shared:
+                self.options['glew'].shared = True
 
     def build(self):
         cmake = CMake(self)
@@ -49,16 +55,19 @@ class Open3dConan(ConanFile):
 
         cmake.definitions["BUILD_EIGEN3"] = False
         cmake.definitions["BUILD_PYBIND11"] = False
-        cmake.definitions["BUILD_GLFW"] = False
-        cmake.definitions["BUILD_GLEW"] = False
-
         cmake.definitions["EIGEN3_FOUND"] = True
-        cmake.definitions["GLEW_FOUND"] = True
-        cmake.definitions["GLFW_FOUND"] = True
+        cmake.definitions["BUILD_PYTHON_MODULE"] = False
+
+        # with_visualization currently only causes open3d to use it's bundled 3rd-party libs
+        # the src/CMakeLists.txt file would need to be patched to disable the complete module.
+
+        if self.options.with_visualization:
+            cmake.definitions["BUILD_GLFW"] = False
+            cmake.definitions["BUILD_GLEW"] = False
+            cmake.definitions["GLEW_FOUND"] = True
+            cmake.definitions["GLFW_FOUND"] = True
 
         cmake.definitions["BUILD_LIBREALSENSE"] = True
-
-        cmake.definitions["BUILD_PYTHON_MODULE"] = False
 
         cmake.configure()
         cmake.build()
